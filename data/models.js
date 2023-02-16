bf22abdfa0d0fd7d0ade1f095defd9c62d7b4743
mmayorga <manuelmayorga@oboria.com>
2023-02-16 12:35:31 +0100

    Initial commit

--- data/models.js
+++ data/models.js
@@ -1,23 +1,6 @@
 export default
     {
         
-        '00-Basic View': './models/00-Basic View/00-Basic View/output.svf',
-        '01-Maple Sugar Moon': './models/01-Maple Sugar Moon/01-Maple Sugar Moon/output.svf',
-        '02-Peeping Frog Moon': './models/02-Peeping Frog Moon/02-Peeping Frog Moon/output.svf',
-        '03-Corn Planting Moon': './models/03-Corn Planting Moon/03-Corn Planting Moon/output.svf',
-        '04-Strawberry Moon': './models/04-Strawberry Moon/04-Strawberry Moon/output.svf',
-        '05-Thunder Moon': './models/05-Thunder Moon/05-Thunder Moon/output.svf',
-        '06-Hot Suns Moon': './models/06-Hot Suns Moon/06-Hot Suns Moon/output.svf',
-        '07-Harvest Moon': './models/07-Harvest Moon/07-Harvest Moon/output.svf',
-        '08-Falling leaves Moon': './models/08-Falling leaves Moon/08-Falling leaves Moon/output.svf',
-        '09-Hunting Moon': './models/09-Hunting Moon/09-Hunting Moon/output.svf',
-        '10-Beaver Moon': './models/10-Beaver Moon/10-Beaver Moon/output.svf',
-        '11-Cold Moon': './models/11-Cold Moon/11-Cold Moon/output.svf',
-        '12-Wolf Moon': './models/12-Wolf Moon/12-Wolf Moon/output.svf',
-        '13-Snow Wading Moon': './models/13-Snow Wading Moon/13-Snow Wading Moon/output.svf',
-        '14-Entry': './models/14-Entry/14-Entry/output.svf',
-        '15-Exit': './models/15-Exit/15-Exit/output.svf',
-        '16-South Control Room': './models/16-South Control Room/16-South Control Room/output.svf',
-        '17-North Control Room': './models/17-North Control Room/17-North Control Room/output.svf',
-        '18_Conduit Distribution': './models/18_Conduit Distribution/18_Conduit Distribution/output.svf'
+        'Tiles': './models/Tiles/Tiles/output.svf',
+        
     }
